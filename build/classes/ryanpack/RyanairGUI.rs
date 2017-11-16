7020de02274944c6a002dda4748a8cfc38699b7a
Zana <zanaadaniel@gmail.com>
2017-11-16 06:07:25 +0000

    first commit

---
+++ build/classes/ryanpack/RyanairGUI.rs
@@ -0,0 +1,1 @@
+Ryanpack.RyanairGUI
---
+++ src/Ryanpack/Ryanair.java
@@ -0,0 +1,10 @@
+package Ryanpack;
+
+public class Ryanair {
+
+    public static void main(String[] args) {
+        RyanairUI GUI = new RyanairUI();
+        GUI.setVisible(true);
+    }
+    
+}
