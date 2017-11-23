02927af7358921e6014bfa32b5bf07b42d8c3ea6
Ayo/Fouad Animashaun <x16300351@student.ncirl.ie>
2017-11-23 12:53:23 +0000

    Small

--- src/Ryanpack/Ryanair.java
+++ src/Ryanpack/Ryanair.java
@@ -3,8 +3,13 @@ package Ryanpack;
 public class Ryanair {
 
     public static void main(String[] args) {
+        //For flight table
         RyanairUI GUI = new RyanairUI();
         GUI.setVisible(true);
+        
+        //User Flight GUI
+        RyanAirUserGUI userGUI = new RyanAirUserGUI();
+        userGUI.setVisible(true);
     }
     
 }
