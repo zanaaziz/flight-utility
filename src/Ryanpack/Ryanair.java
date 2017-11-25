dc70668ff272910dfb2750bbd4eaf9c08f05c723
Zana <zanaadaniel@gmail.com>
2017-11-25 21:18:35 +0000

    changes by ayo reverted

--- src/Ryanpack/Ryanair.java
+++ src/Ryanpack/Ryanair.java
@@ -3,12 +3,7 @@ package Ryanpack;
 public class Ryanair {
 
     public static void main(String[] args) {
-        //Home UI
-//        Home homeUI = new Home();
-//        homeUI.setVisible(true);
-        
-        //Customer UI
-        readFlightsGUI read = new readFlightsGUI();
-        read.setVisible(true);
+        Home homeUI = new Home();
+        homeUI.setVisible(true);
     }
 }
