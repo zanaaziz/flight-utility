148bbabbc8b876a732ffae29bf6cee48c22f2269
Zana <zanaadaniel@gmail.com>
2017-11-29 12:34:17 +0000

    merge

--- src/Ryanpack/AddFlight.java
+++ src/Ryanpack/AddFlight.java
@@ -1,7 +1,5 @@
 package Ryanpack;
 
-import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
 
 public class AddFlight extends javax.swing.JFrame {
     
@@ -154,12 +152,8 @@ public class AddFlight extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void addBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addBtnActionPerformed
-<<<<<<< HEAD
-        
-=======
         func.AddFlight(admin.filePath, admin.table, fromFld, toFld, departureFld, arrivalFld);
         this.dispose();
->>>>>>> 598f39b74ad87bd49e0266d5560442821608357e
     }//GEN-LAST:event_addBtnActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
